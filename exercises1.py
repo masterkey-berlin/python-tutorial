a8e24b527864e3c32703426846b70a0cf166f2a4
masterkey <manuel.huck@edu.techstarter.de>
2024-10-01 16:04:18 +0200

    Zweiter Versuch

---
+++ exercises1.py
@@ -0,0 +1,11 @@
+Die# Definieren der Variablen
+first_name = "Manuel"
+last_name = "Huck" 
+birth_year = "1975"
+has_license = "True"
+
+#Ausgabe der Variablen
+print("first_name:", first_name)
+print("last_name:", last_name)
+print("birth_year:", birth_year)
+print("has_license:", has_license)
